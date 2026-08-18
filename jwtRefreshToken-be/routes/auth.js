1e86a507ea3ce0726bb5419c5aa7010769cf0a22
dk-saber <saber.dkhili@essect.u-tunis.tn>
2026-08-18 21:14:09 +0200

    test conteneurisation

--- jwtRefreshToken-be/routes/auth.js
+++ jwtRefreshToken-be/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const RefreshToken = require('../models/refreshToken');
 const { hashToken, rotateRefreshToken } = require('../utils/tokens');
--- jwtRefreshToken-be/server.js
+++ jwtRefreshToken-be/server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors'); 
 const connectDB = require('./config/db');
 const app = express();
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const cookieParser = require('cookie-parser');
 
+app.use(cors({
+  origin: 'http://localhost:3000',          // l'origine exacte du frontend Nuxt
+  credentials: true                         // autorise l'envoi/réception des cookies
+}));
+
 app.use(express.json());
+app.use(cookieParser());                    // <-- à déplacer AVANT les routes (voir point 3)
 connectDB();
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
